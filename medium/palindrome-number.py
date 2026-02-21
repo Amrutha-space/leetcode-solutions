04bbfc3a851ce2f825bbf67431d5a6146e6c21c8
Amrutha-space <annepuamrutha98@gmail.com>
2026-02-21 11:34:24 +0530

    Add solution for Palindrome Number

---
+++ medium/palindrome-number.py
@@ -0,0 +1,20 @@
+/*
+ * LeetCode Solution: Palindrome Number
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/palindrome-number/submissions/1926009499/
+ * Date: 21/02/2026, 11:34:21
+ * Solution: Initial
+ */
+
+n=n/10;
+        revNum=revNum*10+dig;
+        }
+            return 0;
+        if (revNum > INT_MAX/10 || revNum < INT_MIN/10){
+        int dig = n%10;
+    while(n!=0){
+    int revNum=0;
+int reverse(int n){
+public:
+class Solution {
