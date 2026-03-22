ec825a5ee0874998ff5c6e6794a838bef87e4c4d
Amrutha-space <annepuamrutha98@gmail.com>
2026-03-22 14:49:37 +0530

    Add solution for Flip Square Submatrix Vertically

---
+++ medium/flip-square-submatrix-vertically.py
@@ -0,0 +1,20 @@
+/*
+ * LeetCode Solution: Flip Square Submatrix Vertically
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/flip-square-submatrix-vertically/submissions/1955601658/?envType=daily-question&envId=2026-03-21
+ * Date: 22/03/2026, 14:49:34
+ * Solution: Initial
+ */
+
+for (int j=0; j<k; j++){
+                swap(grid[x+i] [y+j],grid[x+k-i-1] [y+j]);
+            }
+        }
+       return grid;
+    }
+        for(int i =0;i<k/2;i++){
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, 
+int k) {
+public:
+class Solution {
