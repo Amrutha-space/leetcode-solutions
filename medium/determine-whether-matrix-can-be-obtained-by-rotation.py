91c7ad20b0cb51084c66ee9cc6a4369495eaa511
Amrutha-space <annepuamrutha98@gmail.com>
2026-03-22 20:08:50 +0530

    Add solution for Problem Overview

---
+++ medium/determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -0,0 +1,21 @@
+/*
+ * LeetCode Solution: Problem Overview
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/submissions/1955833435/?envType=daily-question&envId=2026-03-22
+ * Date: 22/03/2026, 20:08:48
+ * Solution: Initial
+ */
+
+}
+            }
+                swap(mat[i][j], mat[j][i]);
+            for (int j = i + 1; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+
+        int n = mat.size();
+    void rotate(vector<vector<int>>& mat) {
+    
+public:
+
+        // Reverse rows
