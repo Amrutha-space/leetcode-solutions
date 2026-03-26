63d69d25053b6a42c4d5ba2f0a1870541629f7b3
Amrutha-space <annepuamrutha98@gmail.com>
2026-03-26 22:08:28 +0530

    Add solution for Check if Array Is Sorted and Rotated

---
+++ medium/check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,15 @@
+/*
+ * LeetCode Solution: Check if Array Is Sorted and Rotated
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/submissions/1960117257/
+ * Date: 26/03/2026, 22:08:26
+ * Solution: Initial
+ */
+
+if (count > 1) return false;
+            }
+        }
+        return true;
+    }
+};
