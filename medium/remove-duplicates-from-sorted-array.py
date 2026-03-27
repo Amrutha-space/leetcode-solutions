e14ca54a1dbe9b5c881076cc11a707dbae4dfa0f
Amrutha-space <annepuamrutha98@gmail.com>
2026-03-27 23:02:50 +0530

    Add solution for Remove Duplicates from Sorted Array

---
+++ medium/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,16 @@
+/*
+ * LeetCode Solution: Remove Duplicates from Sorted Array
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1961122522/
+ * Date: 27/03/2026, 23:02:48
+ * Solution: Initial
+ */
+
+nums[i] = nums[j];
+            }
+        }
+
+        return i + 1; 
+    }
+};
