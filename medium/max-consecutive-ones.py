de6b805d91fafdad877117eef50a4df0a817a450
Amrutha-space <annepuamrutha98@gmail.com>
2026-03-31 23:02:53 +0530

    Add solution for Max Consecutive Ones

---
+++ medium/max-consecutive-ones.py
@@ -0,0 +1,20 @@
+/*
+ * LeetCode Solution: Max Consecutive Ones
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/max-consecutive-ones/submissions/1965125731/
+ * Date: 31/03/2026, 23:02:50
+ * Solution: Initial
+ */
+
+}
+                cnt=0;
+            }else{
+                cnt ++;
+        for (int i=0; i< nums.size(); i++) {
+            if (nums[i] == 1){
+        int cnt = 0;
+        int maxi =0;
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+class Solution {
+public:
