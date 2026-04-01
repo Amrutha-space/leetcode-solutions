55a7016f9f616c058bcda0813e85473c4e4eedd0
Amrutha-space <annepuamrutha98@gmail.com>
2026-04-01 22:00:36 +0530

    Add solution for Single Number

---
+++ medium/single-number.py
@@ -0,0 +1,16 @@
+/*
+ * LeetCode Solution: Single Number
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/single-number/submissions/1966027228/
+ * Date: 01/04/2026, 22:00:33
+ * Solution: Initial
+ */
+
+}
+        if (cnt ==1) return num;
+
+       }
+       return -1;
+    }
+};
