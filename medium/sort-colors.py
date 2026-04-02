fcf8e7f59a5762ac2b5cfe80553dad54c7b99351
Amrutha-space <annepuamrutha98@gmail.com>
2026-04-02 21:01:24 +0530

    Add solution for Sort Colors

---
+++ medium/sort-colors.py
@@ -0,0 +1,20 @@
+/*
+ * LeetCode Solution: Sort Colors
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/sort-colors/submissions/1966923492/
+ * Date: 02/04/2026, 21:01:21
+ * Solution: Initial
+ */
+
+mid++;
+        }else if(nums[mid]==1){
+            mid++; low++;
+            swap (nums[low], nums[mid]);
+        if (nums[mid]==0){
+       int low=0, mid=0, high = n-1;
+       while (mid <= high){
+       int n = nums.size();
+    void sortColors(vector<int>& nums) {
+public:
+class Solution {
