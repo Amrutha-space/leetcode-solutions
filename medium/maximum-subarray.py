c2d914e901e8eb7779fd552732ca74e2b90f1820
Amrutha-space <annepuamrutha98@gmail.com>
2026-04-04 22:19:26 +0530

    Add solution for Maximum Subarray

---
+++ medium/maximum-subarray.py
@@ -0,0 +1,15 @@
+/*
+ * LeetCode Solution: Maximum Subarray
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/maximum-subarray/submissions/1968785509/
+ * Date: 04/04/2026, 22:19:22
+ * Solution: Initial
+ */
+
+maxSum = max(maxSum, currSum); 
+        }
+
+        return maxSum;
+    }
+};
