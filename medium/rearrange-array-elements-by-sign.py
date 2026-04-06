514f1c0889fdcaeef3bb2ea65b93cfcb0512750d
Amrutha-space <annepuamrutha98@gmail.com>
2026-04-06 22:43:07 +0530

    Update solution for Rearrange Array Elements by Sign

--- medium/rearrange-array-elements-by-sign.py
+++ medium/rearrange-array-elements-by-sign.py
@@ -1,20 +1,13 @@
-/*
- * LeetCode Solution: Rearrange Array Elements by Sign
- * Difficulty: Medium
- * Language: python
- * URL: https://leetcode.com/problems/rearrange-array-elements-by-sign/submissions/1969807933/
- * Date: 05/04/2026, 22:08:49
- * Solution: Initial
- */
-
-}
-            posIndex += 2;
-            ans[posIndex]=nums[i];
-        if(nums[i]>0){
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+       int n = nums.size();
        vector<int> ans(n, 0);
        int posIndex=0, negIndex=1;
        for(int i=0;i<n; i++){
-    vector<int> rearrangeArray(vector<int>& nums) {
-       int n = nums.size();
-class Solution {
-public:
+        if(nums[i]>0){
+            ans[posIndex]=nums[i];
+            posIndex += 2;
+        }
+
+// Solution 1775495586909
