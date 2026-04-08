0302c818fca0dbcd6718642d9a65fe0b0b410973
Amrutha-space <annepuamrutha98@gmail.com>
2026-04-08 21:27:59 +0530

    Add solution for Longest Consecutive Sequence

---
+++ medium/longest-consecutive-sequence.py
@@ -0,0 +1,20 @@
+/*
+ * LeetCode Solution: Longest Consecutive Sequence
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/longest-consecutive-sequence/submissions/1972739925/
+ * Date: 08/04/2026, 21:27:57
+ * Solution: Initial
+ */
+
+}
+
+        for (int i=0; i<n; i++){
+            st.insert (nums[i]);
+        if(n==0) return 0;
+        int longest = 1;
+        unordered_set <int> st;
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
