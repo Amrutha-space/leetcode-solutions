8cb5253b56552e9bed97ef302c9576bab372a2c3
Amrutha-space <annepuamrutha98@gmail.com>
2026-04-10 20:32:52 +0530

    Add solution for Minimum Distance Between Three Equal Elements I

---
+++ medium/minimum-distance-between-three-equal-elements-i.py
@@ -0,0 +1,20 @@
+/*
+ * LeetCode Solution: Minimum Distance Between Three Equal Elements I
+ * Difficulty: Medium
+ * Language: python
+ * URL: https://leetcode.com/problems/minimum-distance-between-three-equal-elements-i/submissions/1974558232/?envType=daily-question&envId=2026-04-10
+ * Date: 10/04/2026, 20:32:49
+ * Solution: Initial
+ */
+
+mind = min(d , mind);
+                 }
+                 if (nums[i] == nums[j] && nums[j] == nums[k]){
+                    int d = abs(i - j) + abs(j - k) + abs(k - i);
+                for (int k=j+1; k<nums.size(); k++){
+        for (int i=0; i<nums.size(); i++){
+            for (int j=i+1; j<nums.size(); j++){
+    int minimumDistance(vector<int>& nums) {
+        int mind = INT_MAX;
+class Solution {
+public:
