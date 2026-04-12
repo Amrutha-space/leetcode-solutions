e860cecec9e78f026057660501878aa436837d16
Amrutha-space <annepuamrutha98@gmail.com>
2026-04-12 20:30:50 +0530

    Add solution for Longest Palindromic Substring

---
+++ medium/longest-palindromic-substring.c
@@ -0,0 +1,19 @@
+/*
+ * LeetCode Solution: Longest Palindromic Substring
+ * Difficulty: Medium
+ * Language: c
+ * URL: https://leetcode.com/problems/longest-palindromic-substring/submissions/1976495195/
+ * Date: 12/04/2026, 20:30:48
+ * Solution: Initial
+ */
+
+maxLen = right - left + 1;
+                    start = left;
+                if (right - left + 1 > maxLen) {
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            int left = i, right = i;
+        for (int i = 0; i < s.size(); i++) {
+                }
+                left--;
+                right++;
+            }
